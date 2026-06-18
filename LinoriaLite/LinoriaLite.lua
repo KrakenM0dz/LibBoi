7b4c42036563b4e07c6dc3ab6b799422d63ed7e0
KrakenM0dz <135091722+KrakenM0dz@users.noreply.github.com>
2026-06-18 12:43:49 -0400

    Update LinoriaLite.lua

--- LinoriaLite/LinoriaLite.lua
+++ LinoriaLite/LinoriaLite.lua
@@ -26,15 +26,15 @@ function Library:Unload()
 end
 
 Library.Theme = {
-    BackgroundColor = Color3.fromRGB(15/255, 15/255, 15/255),
-    MainColor = Color3.fromRGB(20/255, 20/255, 20/255),
-    GroupBoxColor = Color3.fromRGB(15/255, 15/255, 15/255),
-    OutlineColor = Color3.fromRGB(0/255, 0/255, 0/255),
-    InlineColor = Color3.fromRGB(50/255, 50/255, 50/255),
-    AccentColor = Color3.fromRGB(0/255, 255/255, 255/255),
+    BackgroundColor = Color3.fromRGB(15, 15, 15),
+    MainColor = Color3.fromRGB(20, 20, 20),
+    GroupBoxColor = Color3.fromRGB(15, 15, 15),
+    OutlineColor = Color3.fromRGB(0, 0, 0),
+    InlineColor = Color3.fromRGB(50, 50, 50),
+    AccentColor = Color3.fromRGB(0, 255, 255),
     Font = Enum.Font.Code,
-    TextColor = Color3.fromRGB(255/255, 255/255, 255/255),
-    TextMuted = Color3.fromRGB(150/255, 150/255, 150/255),
+    TextColor = Color3.fromRGB(255, 255, 255),
+    TextMuted = Color3.fromRGB(150, 150, 150),
 }
 
 function Library:UpdateTheme(themeVar, newColor)
